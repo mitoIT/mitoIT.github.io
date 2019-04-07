3b532b8d7a8888d51cf504cdc9f093374d0dbaaa
Jaime Henriquez <jaimeehenriquez@gmail.com>
2019-04-06 22:24:28 -0700

    Updating JS and HTML for Animated SVG

---
+++ index.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+var animData = {
+        container: document.getElementById('bm'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'data.json'
+    };
+    var anim = bodymovin.loadAnimation(animData);
